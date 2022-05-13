aa49c5d5baa35c18c2a3939cc9eb4cc6497cad34
ngzhenghui94 <ngzhenghui94@hotmail.com>
2022-05-14 02:18:09 +0800

    Minor fix

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,4 @@ dotenv.config();
 
 export const channelID = process.env.CHANNELID;
 export const telegramToken = process.env.TOKEN
-export const btcAPI = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=100&page=1"
+export const apiUrl = process.env.APIURL;
