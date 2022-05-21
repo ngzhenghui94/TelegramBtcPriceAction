8be4f4b5198730c9fdb3cc5fc182105d7baf57e9
ngzhenghui94 <ngzhenghui94@hotmail.com>
2022-05-21 13:16:54 +0800

    Added more fields

--- src/msgSender.js
+++ src/msgSender.js
@@ -15,7 +15,10 @@ export const msgSender = async (bot) => {
 		axios.get(apiUrl).then(async (response) => {
 			let price = response.data[0].current_price;
             let percentChange = response.data[0].price_change_percentage_24h;
-			bot.sendMessage(channelID, `⬢ ${process.env.CRYPTOTICKER}/USD Price: ${price}\n ⬢ 24H%: ${percentChange}%`);
+			let marketcap = response.data[0].market_cap;
+			let high24h = response.data[0].high_24h;
+			let low24h = response.data[0].low_24h;
+			bot.sendMessage(channelID, `⬢ ${process.env.CRYPTOTICKER}/USD Price: ${price}\n ⬢ 24H%: ${percentChange}%\n\nmarketcap: ${marketcap}\n24H Low\24H High: ${low24h}/${high24h}`);
 		});
 	});
 };
