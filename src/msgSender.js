8277be5227f55dd503bb781134faaafeb98c0b3b
ngzhenghui94 <ngzhenghui94@hotmail.com>
2022-06-03 19:04:32 +0800

    Removed marketcap, not useful

--- src/msgSender.js
+++ src/msgSender.js
@@ -15,12 +15,12 @@ export const msgSender = async (bot) => {
     axios.get(apiUrl).then(async (response) => {
       let price = response.data[0].current_price;
       let percentChange = response.data[0].price_change_percentage_24h;
-      let marketcap = response.data[0].market_cap == 0 ? "N/A" : response.data[0].market_cap;
+      
       let high24h = response.data[0].high_24h;
       let low24h = response.data[0].low_24h;
       bot.sendMessage(
         channelID,
-        `⬢ ${process.env.CRYPTOTICKER}/USD Price: $${price}\n⬢ 24H%: ${percentChange}%\n\n⬢ Marketcap: $${marketcap}\n⬢ 24H Low/24H High: $${low24h} / $${high24h}`
+        `⬢ ${process.env.CRYPTOTICKER}/USD Price: $${price}\n⬢ 24H%: ${percentChange}%\n⬢ 24H Low/24H High: $${low24h} / $${high24h}`
       );
     });
   });
